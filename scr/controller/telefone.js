ba6a7a05fe6d9644e553ede2286ca952521f093e
Coronel <160642659+helio000@users.noreply.github.com>
2025-03-26 15:27:12 -0300

    Add files via upload

---
+++ scr/controller/telefone.js
@@ -0,0 +1,52 @@
+const { Prismaaluno } = require('@prisma/aluno');
+const prisma = new Prismaaluno();
+
+const create = async (req, res) => {
+    try {
+        const telefone = await prisma.telefone.create({
+            data: req.body
+        });
+        res.status(201).json(telefone).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+const read = async (req, res) => {
+    const telefones = await prisma.telefone.findMany();
+    res.json(telefones);
+}
+
+const update = async (req, res) => {
+    try {
+        const telefone = await prisma.telefone.update({
+            data: req.body,
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(202).json(telefone).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const telefone = await prisma.telefone.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.status(204).json(telefone).end();
+    } catch (e) {
+        res.status(400).json(e).end();
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove
+}
