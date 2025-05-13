3e579303237342ac7eb4fc04aa57b13e1dac3d6c
helio000 <ha2840770@gmail.com>
2025-05-13 16:24:51 -0300

    chamada

---
+++ api/router.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const routes = express.Router();
+
+const Aluno = require('./controllers/aluno');
+const Professores = require('./controllers/professor');
+const Telefone = require('./controllers/telefone');
+//const Turma = require('./controllers/turma');
+const Matricula = require('./controllers/matricula');
+
+
+routes.get('/', (req, res) => {
+  return res.json({ titulo: 'TCC do helio' });
+});
+
+routes.post('/alunos', Aluno.create);
+routes.get('/alunos', Aluno.read);
+routes.get('/alunos/:ra', Aluno.readOne);
+routes.put('/alunos/:ra', Aluno.update);
+routes.delete('/alunos/:ra', Aluno.remove);
+
+routes.post('/professores', Professor.create);
+routes.get('/professores', Professor.read);
+routes.get('/professores/:id', Professor.readOne);
+routes.put('/professores/:id', Professor.update);
+routes.delete('/professores/:id', Professor.remove);
+
+routes.post('/telefone', Telefone.create);
+routes.get('/telefone', Telefone.read);
+routes.get('/telefone/:id', Telefone.readOne);
+routes.put('/telefone/:id', Telefone.update);
+routes.delete('/telefone/:id', Telefone.remove);
+/*
+routes.post('/turmas', Turma.create);
+routes.get('/turmas', Turma.read);
+routes.get('/turmas/:id', Turma.readOne);
+routes.put('/turmas/:id', Turma.update);
+routes.delete('/turmas/:id', Turma.remove);
+*/
+
+routes.post('/matricula', Matricula.create);
+routes.get('/matricula', Matricula.read);
+routes.get('/matricula/:id', Matricula.readOne);
+routes.put('/matricula/:id', Matricula.update);
+routes.delete('/matricula/:id', Matricula.remove);
+
+module.exports = routes;
--- api/server.js
+++ api/server.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const routes = require('./src/router');
 
-const PORT = process.env.PORT || 3000
-const router = require('./src/router');
+const PORT = process.env.PORT || 3100;
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
+app.use(routes);
 
-app.listen(PORT,()=>{
-    console.log('API respondendo em http://localhost:'+PORT);
+app.listen(PORT, () => {
+    console.log(`API rodando em http://localhost:${PORT}`);
 });
