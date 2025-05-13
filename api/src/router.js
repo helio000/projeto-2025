de118d454c8e2c005630354835d9f2f4754b765a
Coronel <160642659+helio000@users.noreply.github.com>
2025-05-13 15:45:58 -0300

    router.js

--- api/src/router.js
+++ api/src/router.js
@@ -1,15 +1,48 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Router();
 
 const Aluno = require('./controllers/aluno');
+//const Professor = require('./controllers/professor');
+const Telefone = require('./controllers/telefone');
+//const Turma = require('./controllers/turma');
+const Matricula = require('./controllers/matricula');
 
-router.get('/',(req, res)=>{
-    res.json({titulo:'TCC SENAI API'});
+routes.get('/', (req, res) => {
+  return res.json({ titulo: 'Escola Superior PW' });
 });
 
-// router.post('/alunos',Aluno.create);
-// router.get('/alunos',Aluno.read);
-// router.patch('/alunos/:id',Aluno.update);
-// router.delete('/alunos/:id',Aluno.remove);
 
-module.exports = router;
+routes.post('/alunos', Aluno.create);
+routes.get('/alunos', Aluno.read);
+routes.get('/alunos/:ra', Aluno.readOne);
+routes.put('/alunos/:ra', Aluno.update);
+routes.delete('/alunos/:ra', Aluno.remove);
+
+/*
+routes.post('/professores', Professor.create);
+routes.get('/professores', Professor.read);
+routes.get('/professores/:id', Professor.readOne);
+routes.put('/professores/:id', Professor.update);
+routes.delete('/professores/:id', Professor.remove);
+*/
+
+routes.post('/telefone', Telefone.create);
+routes.get('/telefone', Telefone.read);
+routes.get('/telefone/:id', Telefone.readOne);
+routes.put('/telefone/:id', Telefone.update);
+routes.delete('/telefone/:id', Telefone.remove);
+/*
+routes.post('/turmas', Turma.create);
+routes.get('/turmas', Turma.read);
+routes.get('/turmas/:id', Turma.readOne);
+routes.put('/turmas/:id', Turma.update);
+routes.delete('/turmas/:id', Turma.remove);
+*/
+
+routes.post('/matricula', Matricula.create);
+routes.get('/matricula', Matricula.read);
+routes.get('/matricula/:id', Matricula.readOne);
+routes.put('/matricula/:id', Matricula.update);
+routes.delete('/matricula/:id', Matricula.remove);
+
+module.exports = routes;
